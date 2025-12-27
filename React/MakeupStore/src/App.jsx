f49e081e1bf32e8a2f4183a7bf2fee63cf48e6e0
Kritika1380 <kritika1380@gmail.com>
2025-12-27 20:36:24 +0530

    Tailwindcss

---
+++ React/MakeupStore/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/product" element={<Product/>} />
+      <Route path="/contact" element={<Contact/>} />
+    </Routes>
+    </BrowserRouter>
+    
+     
+    </>
+  );
+}
+
+export default App;
---
+++ React/MakeupStore/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+      <div className="flex bg-amber-600 justify-between py-2">
+        <h1 className="text-4xl">MakeupStore</h1>
+        <div className="flex gap-3 justify-center items-center">
+          <Link to={"/"} className=" flex text-white items-center gap-1">
+            <FaHome /> Home
+          </Link>
+          <Link to={"/about"} className="text-white">
+            About
+          </Link>
+          <Link to={"/product"} className="text-white">
+            Product
+          </Link>
+          <Link to={"/contact"} className="text-white">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
