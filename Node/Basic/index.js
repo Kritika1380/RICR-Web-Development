1fcee9e9f5b33b529021a1a3c5fdd18b4ceef018
Kritika1380 <kritika1380@gmail.com>
2026-01-06 20:03:53 +0530

    nodes

---
+++ Node/Basic/index.js
@@ -0,0 +1,24 @@
+// console.log('hello world');
+
+// let a = 10;
+
+// if(a%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+function add(a, b) {
+  console.log(a + b);
+}
+function sub(a, b) {
+  console.log(a - b);
+}
+function mul(a, b) {
+  console.log(a * b);
+}
+
+console.log("abcd");
+
+// export {add , sub};
+export default mul;
---
+++ Node/Basic/index2.js
@@ -0,0 +1,11 @@
+import add from "./index.js"
+// import {sub} from "./index.js"
+// import { add, sub } from "./index.js";
+// import kuchBhi from "./index.js";
+
+let x = 10;
+let y = 20;
+
+add(x, y);
+// sub(y, x);
+// kuchBhi(x, y);
