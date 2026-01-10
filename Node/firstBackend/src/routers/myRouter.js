6e6435c23910367a841a18aaf612d6334b28a23c
Kritika1380 <kritika1380@gmail.com>
2026-01-10 20:31:01 +0530

    Middleware, HTTP Put method.

--- Node/firstBackend/src/routers/myRouter.js
+++ Node/firstBackend/src/routers/myRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { UserRegister,UserLogin,UserLogout } from '../controllers/myController.js';
+import { UserRegister,UserLogin,UserLogout, UserUpdate } from '../controllers/myController.js';
 
 const router = express.Router();
 
 router.post("/register",UserRegister);//function callback hoga
 router.post("/login",UserLogin);
-router.post("/logout",UserLogout);
+router.get("/logout",UserLogout);
+router.put("/update",UserUpdate);
 
 
 export default router;
